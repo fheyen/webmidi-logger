654b3304dcc519f3a0599136bd33e4c966b9593d
fheyen <frank.heyen@outlook.de>
2020-10-26 21:19:09 +0100

    Updates

---
+++ precache-manifest.134aa6365831cc908d89221dccbb7b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a6b590f467eae9c9e67977ddeec7ff",
+    "url": "/webmidi-logger/index.html"
+  },
+  {
+    "revision": "0684eca8114bea8737d0",
+    "url": "/webmidi-logger/static/css/main.36982d05.chunk.css"
+  },
+  {
+    "revision": "6bc5ade2bd98e4e85aa3",
+    "url": "/webmidi-logger/static/js/2.80370c18.chunk.js"
+  },
+  {
+    "revision": "0cdebb9bd8921e6ffed16c110c74fb19",
+    "url": "/webmidi-logger/static/js/2.80370c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0684eca8114bea8737d0",
+    "url": "/webmidi-logger/static/js/main.45d4de15.chunk.js"
+  },
+  {
+    "revision": "2c9f57d164e7aaae6505",
+    "url": "/webmidi-logger/static/js/runtime-main.81e2c203.js"
+  }
+]);
