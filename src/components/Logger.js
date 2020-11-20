8d78ee6add47a449927fcd4bfd1a976cf4c71bce
Frank Heyen <heyenfk@visus.uni-stuttgart.de>
2020-11-20 15:25:06 +0100

    logger now supports filtering by device, channel, and command and shows newest messages on top with a limit

--- src/components/Logger.js
+++ src/components/Logger.js
@@ -9,7 +9,16 @@ export default class Logger extends Component {
         super(props);
         this.state = {
             midiInputs: [],
-            messages: []
+            messages: [],
+            // Filter options
+            devices: new Set(),
+            channels: new Set(),
+            commands: new Set(),
+            // Filter options set
+            hiddenDevices: new Set(),
+            hiddenChannels: new Set(),
+            hiddenCommands: new Set()
+
         };
         this.commands = new Map([
             [128, 'noteOff'],
@@ -62,58 +71,82 @@ export default class Logger extends Component {
      * @param {*} message MIDI message
      */
     _handleMIDIMessage = (message) => {
-        console.log(message);
+        // console.log(message);
         const device = message.target.name;
         const commandAndChannel = message.data[0];
         const channel = commandAndChannel % 16;
         const command = commandAndChannel - channel;
+        const commandName = this.commands.get(command) || 'unknownCommand';
         const time = message.timeStamp;
         const pitch = message.data[1];
         // A velocity value might not be included with a noteOff command
         const velocity = (message.data.length > 2) ? message.data[2] : 0;
-        const commandName = this.commands.get(command) || 'unknownCommand';
-        this.state.messages.push((
-            <div
-                className={`message ${commandName}`}
-                key={this.state.messages.length}
-                title='Click to log the object to the browser console'
-                onClick={() => console.log(message)}
-            >
-                <div className='id' title='Number'>
-                    {this.state.messages.length}
-                </div>
-                <div className='time' title={`Time: ${time}`}>
-                    {Math.floor(time)}
-                </div>
-                <div className='device' title={`Device: ${device}`}>
-                    {device.slice(0, 30)}
-                </div>
-                <div className='channel' title='Channel'>
-                    {channel}
-                </div>
-                <div className='command' title='Command'>
-                    {command} {commandName}
-                </div>
-                {[128, 144].includes(command) && (
-                    <div className='pitch' title='Pitch'>
-                        {pitch} {getMidiNoteByNr(pitch).label}
-                    </div>
-                )}
-                {[144].includes(command) && (
-                    <div className='velocity' title='Velocity'>
-                        {velocity}
-                    </div>
-                )}
-            </div>
-        ));
-        this.setState({ messages: this.state.messages });
+        const messageObject = {
+            message,
+            id: this.state.messages.length,
+            device,
+            channel,
+            command,
+            commandName,
+            time,
+            pitch,
+            velocity
+        };
+        // Update sets of existing devices, channels, commands
+        const devices = new Set(this.state.devices).add(device);
+        const channels = new Set(this.state.channels).add(channel);
+        const commands = new Set(this.state.commands).add(command);
+        this.setState({
+            messages: this.state.messages.concat(messageObject),
+            devices,
+            channels,
+            commands
+        });
+    }
+
+    toggleDevice = (e) => {
+        const { hiddenDevices } = this.state;
+        const value = e.target.value;
+        const updated = new Set(hiddenDevices);
+        if (hiddenDevices.has(value)) {
+            updated.delete(value);
+        } else {
+            updated.add(value);
+        }
+        this.setState({ hiddenDevices: updated });
+    }
+
+    toggleChannel = (e) => {
+        const { hiddenChannels } = this.state;
+        const value = +e.target.value;
+        const updated = new Set(hiddenChannels);
+        if (hiddenChannels.has(value)) {
+            updated.delete(value);
+        } else {
+            updated.add(value);
+        }
+        this.setState({ hiddenChannels: updated });
+    }
+
+    toggleCommand = (e) => {
+        const { hiddenCommands } = this.state;
+        const value = +e.target.value;
+        const updated = new Set(hiddenCommands);
+        if (hiddenCommands.has(value)) {
+            updated.delete(value);
+        } else {
+            updated.add(value);
+        }
+        this.setState({ hiddenCommands: updated });
     }
 
     render() {
-        const inputs = [];
-        for (let input of this.state.midiInputs) {
-            console.log(input.name);
+        const { messages, midiInputs, devices, channels, commands, hiddenDevices, hiddenChannels, hiddenCommands } = this.state;
 
+        // Log and show inputs
+        const inputs = [];
+        for (let input of midiInputs) {
+            // console.log(input.name);
             inputs.push((
                 <div
                     key={input.name}
@@ -125,21 +158,113 @@ export default class Logger extends Component {
                 </div>
             ));
         }
-        console.log(this.state.midiInputs);
-        console.log(inputs);
+        // console.log(midiInputs);
+
+        // Filter message sby user defined filters
+        const filteredMessages = messages.filter(d => {
+            const { device, channel, command } = d;
+            if (hiddenDevices.has(device) || hiddenChannels.has(channel) || hiddenCommands.has(command)) {
+                return false;
+            }
+            return true;
+        });
+
+        // Create JSX for messages
+        const nShown = 50;
+        const messageElements = filteredMessages
+            .reverse()
+            .slice(0, nShown)
+            .map(d => (
+                <div
+                    className={`message ${d.commandName}`}
+                    key={d.id}
+                    title='Click to log the object to the browser console'
+                    onClick={() => console.log(d)}
+                >
+                    <div className='id' title='Number'>
+                        {d.id}
+                    </div>
+                    <div className='time' title={`Time: ${d.time}`}>
+                        {Math.floor(d.time)}
+                    </div>
+                    <div className='device' title={`Device: ${d.device}`}>
+                        {d.device.slice(0, 30)}
+                    </div>
+                    <div className='channel' title='Channel'>
+                        {d.channel}
+                    </div>
+                    <div className='command' title='Command'>
+                        {d.command} {d.commandName}
+                    </div>
+                    {[128, 144].includes(d.command) && (
+                        <div className='pitch' title='Pitch'>
+                            {d.pitch} {getMidiNoteByNr(d.pitch).label}
+                        </div>
+                    )}
+                    {[144].includes(d.command) && (
+                        <div className='velocity' title='Velocity'>
+                            {d.velocity}
+                        </div>
+                    )}
+                </div>
+            ));
 
         return (
             <div className='Logger'>
                 <div>
-                    <h2>{this.state.midiInputs.length} MIDI Inputs</h2>
+                    <h2>{midiInputs.length} MIDI Input{midiInputs.length > 1 && 's'}</h2>
                     <div>
                         {inputs}
                     </div>
                 </div>
                 <div>
                     <h2>
-                        {this.state.messages.length} messages
+                        {messages.length} messages, {filteredMessages.length} with current filter, showing newest on top, limited to {nShown}
                     </h2>
+                    <div className='filterOptions'>
+                        Devices
+                        <div>
+                            {Array.from(devices).sort().map(d => (
+                                <button
+                                    key={d}
+                                    value={d}
+                                    onClick={this.toggleDevice}
+                                    className={hiddenDevices.has(d) ? 'hidden' : 'shown'}
+                                    title='Click to hide / show'
+                                >
+                                    {d}
+                                </button>
+                            ))}
+                        </div>
+                        Channels
+                        <div>
+                            {Array.from(channels).sort((a, b) => a - b).map(d => (
+                                <button
+                                    key={d}
+                                    value={d}
+                                    onClick={this.toggleChannel}
+                                    className={hiddenChannels.has(d) ? 'hidden' : 'shown'}
+                                    title='Click to hide / show'
+                                >
+                                    Channel {d}
+                                </button>
+                            ))}
+                        </div>
+                        Commands
+                        <div>
+                            {Array.from(commands).sort((a, b) => a - b).map(d => (
+                                <button
+                                    key={d}
+                                    value={d}
+                                    onClick={this.toggleCommand}
+                                    className={hiddenCommands.has(d) ? 'hidden' : 'shown'}
+                                    title='Click to hide / show'
+                                >
+                                    {d} ({this.commands.get(d) || 'unknownCommand'})
+                                </button>
+                            ))}
+                        </div>
+                    </div>
                     <div className='message'>
                         <div className='id'>#</div>
                         <div className='time'>Time (ms)</div>
@@ -149,8 +274,8 @@ export default class Logger extends Component {
                         <div className='pitch'>Note</div>
                         <div className='velocity'>Velocity</div>
                     </div>
-                    <div>
-                        {this.state.messages}
+                    <div className='messagesContainer'>
+                        {messageElements}
                     </div>
                 </div>
             </div>
